d6cd6613488946d0131d593bd01383d2e4f79a38
RoadToDataGCP <road.to.data.gcp@gmail.com>
2025-04-30 16:09:31 +0200

    Add files via upload

---
+++ pruebagcpjavi/main.py
@@ -0,0 +1,10 @@
+import time
+
+def escribe_hora():
+    print("Escribiendo la hora...")
+    hora = time.strftime("%H:%M:%S")
+    print(f"La hora es: {hora}")
+
+if __name__ == "__main__":
+    escribe_hora()
+    print("Fin del programa.")
