887b73ef2deb2d976486e9254f7f35dea06929ae
javi97ruiz <javier.ruiz.serrano59@gmail.com>
2025-04-30 17:24:56 +0200

    solucion error

--- pruebagcpjavi/main.py
+++ pruebagcpjavi/main.py
@@ -1,19 +1,10 @@
-from flask import Flask
-
-import os
- 
-app = Flask(__name__)
- 
-@app.route("/")
+import time
 
 def escribe_hora():
+    print("Escribiendo la hora...")
+    hora = time.strftime("%H:%M:%S")
+    print(f"La hora es: {hora}")
 
-    print("Hola mundo")
- 
 if __name__ == "__main__":
-
-    port = int(os.environ.get("PORT", 8080))
-
-    app.run(host="0.0.0.0", port=port)
-
- 
+    escribe_hora()
+    print("Fin del programa.")
